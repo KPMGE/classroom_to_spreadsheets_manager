20c28dbf06e282d047c6d2998167e67564285263
vortex2jm <joaopaulomoura5328@gmail.com>
2022-06-14 00:39:52 -0300

    adding objects to paint cells and your request :arrow_up:

---
+++ objects/colorCell.py
@@ -0,0 +1,29 @@
+body = {
+            "requests": [
+                {
+                    "updateCells": {
+                        "rows": {
+                            "values": [
+                                {
+                                    "userEnteredFormat": {
+                                        "backgroundColor":{
+                                            "red": 100.0,
+                                            "green": 0.0,
+                                            "blue": 0.0
+                                        }
+                                    },
+                                    "userEnteredValue":{
+                                        "stringValue": "Jaozim"
+                                    }
+                                }
+                            ]
+                        },
+                        "fields": "*",
+                        "start":{
+                            "rowIndex": 4,
+                            "columnIndex": 4
+                        }
+                    }
+                }
+            ]
+        }
---
+++ objects/colorLine.py
@@ -0,0 +1,23 @@
+body = {
+            "requests":[
+                {
+                    "repeatCell": {
+                        "range": {                    
+                            "startRowIndex": 0,
+                            "endRowIndex": 5
+                        },
+                        "cell": {  
+                            "userEnteredFormat": {
+                                "backgroundColor": {
+                                    "red": 255.0,
+                                    "green": 255.0,
+                                    "blue": 255.0
+                                }
+                            }
+                        },
+                        "fields": "userEnteredFormat(backgroundColor)"
+                    }
+                }
+            ],
+            "includeSpreadsheetInResponse": "false" 
+        }
